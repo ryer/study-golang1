567578b2a321ca0878c72c0bd7bf13c449d49cd9
ryer <ryer@users.noreply.github.com>
2019-01-28 10:08:57 +0900

    fmt

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ var (
 func main() {
 	var (
 		urlListJsonFile = flag.String("url-list", "", "画像カウント先URLのJSONデータファイル")
-		socksPort      = flag.Int("socks-port", 0, "SOCKSサーバのポート番号")
+		socksPort       = flag.Int("socks-port", 0, "SOCKSサーバのポート番号")
 		useMonkey       = flag.Bool("use-monkey", false, "モンキー言語を使う")
 		echoPort        = flag.Int("echo-port", 0, "ECHOサーバのポート番号")
 	)
